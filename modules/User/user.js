543bb5203e9d367229ae392353fef108dae37cb9
ɭê ɦùռɠ <125887825+nguoilaisaochoi@users.noreply.github.com>
2024-09-04 13:59:54 +0700

    v1.5.3

--- modules/User/user.js
+++ modules/User/user.js
@@ -3,9 +3,10 @@ const Schema = mongoose.Schema;
 
 const userschema = new Schema(
   {
+    idToken: { type: String },
     name: { type: String, require: true },
     username: { type: String, require: true },
-    password: { type: String, require: true },
+    password: { type: String },
   },
   {
     timestamps: true,
