24ac96b0ea8220b1c4b9219e45eea3fcc2dd824d
ɭê ɦùռɠ <125887825+nguoilaisaochoi@users.noreply.github.com>
2024-10-07 09:43:14 +0700

    1.5.9.2

--- config/socket.js
+++ config/socket.js
@@ -1,4 +1,8 @@
 const server = require("http").createServer();
-const io = require("socket.io")(server);
-
+const io = require("socket.io")(server, {
+    cors: {
+      origin: "*",
+    },
+  });
+  
 module.exports = { io, server };
