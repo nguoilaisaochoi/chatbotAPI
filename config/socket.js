20507a2d9ab62c2886cd1af0eae7610e279ea867
ɭê ɦùռɠ <125887825+nguoilaisaochoi@users.noreply.github.com>
2024-10-07 10:08:01 +0700

    v1.5.9.4

--- config/socket.js
+++ config/socket.js
@@ -1,9 +1,11 @@
 const server = require("http").createServer();
 const io = require("socket.io")(server, {
-    cors: {
-      origin: "*",
-    },
-  });
-  
+  cors: {
+    origin: "*",
+  },
+});
+server.listen(3000, () => {
+    console.log('Server đang chay tren cong 3000');
+});
 
 module.exports = { io, server };
